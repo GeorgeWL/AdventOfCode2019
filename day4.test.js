f1dcbca7f1cdad5ced6cdf01f5444d1bd370beef
GeorgeWL <george.langham@ivendi.com>
2019-12-05 12:17:59 +0000

    more tests. further codesplitting

--- day4.test.js
+++ day4.test.js
@@ -9,8 +9,8 @@
 
 const CASE_SIMPLE = '111111';
 const CASE_SIMPLE_ASC = '112345';
-const CASE_FAILURE_DECREASE = '223450';
-const CASE_FAILURE_DECREASE_DOUBLE = '111110';
+const CASE_FAILURE_DECREASE = '123450';
+const CASE_FAILURE_DECREASE_DOUBLE = '223450';
 const CASE_FAILURE_NO_DOUBLE = '123456';
 const CASE_FAILURE_LENGTH = '12345';
 
@@ -22,8 +22,11 @@ async function searchPasswords(input) {
 		reason = 'Expect Input Length of 6';
 		return { containsPass, reason, matches };
 	} else {
-		const dupeCheck = Array.from(await checkPasswordRules(input));
-		containsPass = dupeCheck.length >= 1;
+		const dupeCheck = await checkPasswordRules(input);
+		containsPass = dupeCheck.filter(res => res !== false).length >= 1;
+		if (!checkNotDescending(input)) {
+			containsPass = false;
+		}
 		matches = dupeCheck;
 		reason = containsPass
 			? 'Contains Two or More Matching Adjacent'
@@ -32,33 +35,45 @@ async function searchPasswords(input) {
 	}
 }
 
-async function checkPasswordRules(input) {
+function checkPasswordRules(input) {
 	let prevDigit;
 	let currentDigit;
-	const matches = new Set();
-	return new Promise((resolve, reject) => {
-		const isAscending = checkAscending(input);
-		!isAscending && resolve();
-		input.split('').forEach((digit, index) => {
-			currentIndex = index++;
-			currentDigit = digit;
-			checkMatchPair(prevDigit, currentDigit) &&
-				matches.add(`${prevDigit}${currentDigit}`);
-			prevDigit = digit;
-			matches.clear();
-			if (currentIndex === input.length) {
-				resolve(matches);
-			}
-		});
+	let currentIndex = 0;
+	input = String(input);
+	const results = input.split('').map((digit, index) => {
+		currentIndex = index + 1;
+		currentDigit = Number(digit);
+
+		if (checkMatchPair(prevDigit, currentDigit) === true) {
+			const res = `${prevDigit}${currentDigit}`;
+			prevDigit = Number(digit);
+			return res;
+		} else {
+			return false;
+		}
 	});
+	if (currentIndex === input.length) {
+		return results;
+	}
 }
 
 function checkMatchPair(prevDigit, currentDigit) {
 	return currentDigit === prevDigit;
 }
 
-function checkAscending(input) {
-	return false;
+function checkNotDescending(input) {
+	const numbers = String(input)
+		.split('')
+		.map(int => Number(int));
+	let lastGood = 0;
+	const res = numbers.map(int => {
+		const isgood = int >= lastGood;
+		if (isgood) {
+			lastGood = int;
+		}
+		return isgood;
+	});
+	return res.every(int => int === true);
 }
 
 async function passwordsInRange(start, end) {
@@ -69,18 +84,41 @@ async function passwordsInRange(start, end) {
 		.map(async i => await searchPasswords(i.toString()));
 	return Promise.all(results);
 }
+
 jest.setTimeout(120000);
 
 describe('simple it', () => {
-	describe.only('is double its', () => {
+	describe('is double its', () => {
 		it('should return false double', () => {
 			const res = checkMatchPair(1, 2);
+			expect(res).toBe(false);
+		});
+		it('should return false double', () => {
+			const res = checkMatchPair(3, 2);
 			expect(res).toBe(false);
 		});
 
 		it('should return true double', () => {
 			const res = checkMatchPair(1, 1);
 			expect(res).toBe(true);
+		});
+	});
+	describe('isAscendingOnly', () => {
+		it('should return true not descending - CASE_SIMPLE', () => {
+			const res = checkNotDescending(CASE_SIMPLE);
+			expect(res).toBe(true);
+		});
+		it('should return true not descending - CASE_SIMPLE_ASC', () => {
+			const res = checkNotDescending(CASE_SIMPLE_ASC);
+			expect(res).toBe(true);
+		});
+		it('should return false not descending', () => {
+			const res = checkNotDescending(CASE_FAILURE_DECREASE);
+			expect(res).toBe(false);
+		});
+		it('should return false not descending', () => {
+			const res = checkNotDescending(CASE_FAILURE_DECREASE_DOUBLE);
+			expect(res).toBe(false);
 		});
 	});
 	describe('fail tests searchPasswords', () => {
@@ -105,23 +143,26 @@ describe('simple it', () => {
 			expect(res.containsPass).toBe(false);
 		});
 	});
-
-	it('should pass double - all same digit', async () => {
-		const res = await searchPasswords(CASE_SIMPLE);
-		expect(res.containsPass).toBe(true);
+	describe('final results tests', () => {
+		it('should pass double - all same digit', async () => {
+			const res = await searchPasswords(CASE_SIMPLE);
+			expect(res.containsPass).toBe(true);
+		});
+		it('should pass double - one pair', async () => {
+			const res = await searchPasswords(CASE_SIMPLE_ASC);
+			expect(res.containsPass).toBe(true);
+		});
 	});
-	it('should pass double - one pair', async () => {
-		const res = await searchPasswords(CASE_SIMPLE_ASC);
-		expect(res.containsPass).toBe(true);
-	});
-	it('get passwords in range', async () => {
-		const res = await passwordsInRange(111110, 111121);
-		expect(res).toHaveLength(12);
-		expect(res.filter(item => item.containsPass)).toHaveLength(9);
+	describe('get in range tests', () => {
+		it('get passwords in range', async () => {
+			const res = await passwordsInRange(111110, 111121);
+			expect(res).toHaveLength(12);
+			expect(res.filter(item => item.containsPass)).toHaveLength(9);
+		});
 	});
 });
 
-passwordsInRange(254032, 789860).then(res => {
-	console.log(res.filter(item => item.containsPass).length);
-	return res;
-});
+// passwordsInRange(254032, 789860).then(res => {
+// 	console.log(res.filter(item => item.containsPass).length);
+// 	return res;
+// });
