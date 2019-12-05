0010da820be7f1690ba998577a819f3bc73ddaf2
GeorgeWL <george.langham@ivendi.com>
2019-12-05 14:08:43 +0000

    all tests pass. wrong value still

--- day4.test.js
+++ day4.test.js
@@ -7,54 +7,34 @@
  * Only ever Increase or stay the same
  */
 
-const CASE_SIMPLE = '111111';
-const CASE_SIMPLE_ASC = '112345';
-const CASE_FAILURE_DECREASE = '123450';
-const CASE_FAILURE_DECREASE_DOUBLE = '223450';
-const CASE_FAILURE_NO_DOUBLE = '123456';
-const CASE_FAILURE_LENGTH = '12345';
+const CASE_SIMPLE = 111111;
+const CASE_SIMPLE_ASC = 112345;
+const CASE_FAILURE_DECREASE = 123450;
+const CASE_FAILURE_DECREASE_DOUBLE = 223450;
+const CASE_FAILURE_NO_DOUBLE = 123456;
+const CASE_FAILURE_LENGTH = 12345;
 
-async function searchPasswords(input) {
-	let containsPass = false;
-	let matches;
-	let reason;
+function passwordMeetsCriteria(input) {
+	input = input.toString();
 	if (input.length !== 6) {
-		reason = 'Expect Input Length of 6';
-		return { containsPass, reason, matches };
+		return false;
 	} else {
-		const dupeCheck = await checkPasswordRules(input);
-		containsPass = dupeCheck.filter(res => res !== false).length >= 1;
-		if (!checkNotDescending(input)) {
-			containsPass = false;
-		}
-		matches = dupeCheck;
-		reason = containsPass
-			? 'Contains Two or More Matching Adjacent'
-			: 'Does not contain pass';
-		return { containsPass, reason, matches };
+		return findDupesInPassword(input) && checkNotDescending(input);
 	}
 }
 
-function checkPasswordRules(input) {
-	let prevDigit;
-	let currentDigit;
-	let currentIndex = 0;
-	input = String(input);
-	const results = input.split('').map((digit, index) => {
-		currentIndex = index + 1;
-		currentDigit = Number(digit);
-
-		if (checkMatchPair(prevDigit, currentDigit) === true) {
-			const res = `${prevDigit}${currentDigit}`;
-			prevDigit = Number(digit);
-			return res;
-		} else {
-			return false;
-		}
-	});
-	if (currentIndex === input.length) {
-		return results;
-	}
+function findDupesInPassword(input) {
+	input = input.toString();
+	const chunks = [
+		[input[0], input[1]],
+		[input[2], input[3]],
+		[input[4], input[5]]
+	];
+	return chunks
+		.map(chunk => {
+			return checkMatchPair(chunk[0], chunk[1]);
+		})
+		.some(res => res === true);
 }
 
 function checkMatchPair(prevDigit, currentDigit) {
@@ -76,13 +56,23 @@ function checkNotDescending(input) {
 	return res.every(int => int === true);
 }
 
-async function passwordsInRange(start, end) {
-	const results = Array(end - start + 1)
+function passwordsInRange(start, end) {
+	const passArray = Array(end - start + 1)
 		.fill()
 		.map((_, index) => start + index)
-		.fill(start, end)
-		.map(async i => await searchPasswords(i.toString()));
-	return Promise.all(results);
+		.fill(start, end);
+	const validPasswords = [];
+	passArray.forEach(pass => {
+		if (passwordMeetsCriteria(pass)) {
+			validPasswords.push(pass);
+		}
+	});
+	console.log({
+		validPasses: validPasswords.length,
+		passwordsChecked: passArray.length
+	});
+
+	return validPasswords;
 }
 
 jest.setTimeout(120000);
@@ -97,15 +87,23 @@ describe('simple it', () => {
 			const res = checkMatchPair(3, 2);
 			expect(res).toBe(false);
 		});
-
 		it('should return true double', () => {
 			const res = checkMatchPair(1, 1);
 			expect(res).toBe(true);
 		});
 	});
+
 	describe('isAscendingOnly', () => {
 		it('should return true not descending - CASE_SIMPLE', () => {
 			const res = checkNotDescending(CASE_SIMPLE);
+			expect(res).toBe(true);
+		});
+		it('should return true not descending - CASE_SIMPLE_ASC', () => {
+			const res = checkNotDescending(CASE_FAILURE_LENGTH);
+			expect(res).toBe(true);
+		});
+		it('should return true not descending - CASE_SIMPLE_ASC', () => {
+			const res = checkNotDescending(CASE_FAILURE_NO_DOUBLE);
 			expect(res).toBe(true);
 		});
 		it('should return true not descending - CASE_SIMPLE_ASC', () => {
@@ -121,48 +119,45 @@ describe('simple it', () => {
 			expect(res).toBe(false);
 		});
 	});
+
 	describe('fail tests searchPasswords', () => {
-		it('should fail if length !==6', async () => {
-			const res = await searchPasswords(CASE_FAILURE_LENGTH);
-			expect(res.reason).toBe('Expect Input Length of 6');
-			expect(res.containsPass).toBe(false);
+		it('should fail if length !==6', () => {
+			const res = passwordMeetsCriteria(CASE_FAILURE_LENGTH);
+			expect(res).toBe(false);
 		});
-		it('should fail if later descend', async () => {
-			const res = await searchPasswords(CASE_FAILURE_DECREASE);
-			expect(res.reason).toBe('Does not contain pass');
-			expect(res.containsPass).toBe(false);
+		it('should fail if later descend - no double', () => {
+			const res = passwordMeetsCriteria(CASE_FAILURE_DECREASE);
+			expect(res).toEqual(false);
 		});
-		it('should fail if later descend', async () => {
-			const res = await searchPasswords(CASE_FAILURE_DECREASE_DOUBLE);
-			expect(res.reason).toBe('Does not contain pass');
-			expect(res.containsPass).toBe(false);
+		it('should fail if later descend - has double', () => {
+			const res = passwordMeetsCriteria(CASE_FAILURE_DECREASE_DOUBLE);
+			expect(res).toEqual(false);
 		});
-		it('should fail if no double', async () => {
-			const res = await searchPasswords(CASE_FAILURE_NO_DOUBLE);
-			expect(res.reason).toBe('Does not contain pass');
-			expect(res.containsPass).toBe(false);
+		it('should fail if no double', () => {
+			const res = passwordMeetsCriteria(CASE_FAILURE_NO_DOUBLE);
+			expect(res).toEqual(false);
 		});
 	});
-	describe('final results tests', () => {
-		it('should pass double - all same digit', async () => {
-			const res = await searchPasswords(CASE_SIMPLE);
-			expect(res.containsPass).toBe(true);
+
+	describe('find dupes', () => {
+		it('should pass double - all same digit', () => {
+			const res = findDupesInPassword(CASE_SIMPLE);
+			expect(res).toBe(true);
 		});
-		it('should pass double - one pair', async () => {
-			const res = await searchPasswords(CASE_SIMPLE_ASC);
-			expect(res.containsPass).toBe(true);
+		it('should pass double - one pair', () => {
+			const res = findDupesInPassword(CASE_SIMPLE_ASC);
+			expect(res).toBe(true);
 		});
 	});
+
 	describe('get in range tests', () => {
-		it('get passwords in range', async () => {
-			const res = await passwordsInRange(111110, 111121);
-			expect(res).toHaveLength(12);
-			expect(res.filter(item => item.containsPass)).toHaveLength(9);
+		it('get passwords in range', () => {
+			const res = passwordsInRange(111110, 111121);
+			expect(res).toHaveLength(9);
+		});
+		it('get passwords in range', () => {
+			const res = passwordsInRange(254032, 789860);
+			expect(res).toHaveLength(936);
 		});
 	});
 });
-
-// passwordsInRange(254032, 789860).then(res => {
-// 	console.log(res.filter(item => item.containsPass).length);
-// 	return res;
-// });
